21ad3d6aa17799ca456475d5916fc4497eb6b33e
Luis Fernando <luisfernando.mourag@gmail.com>
2023-12-11 21:13:57 -0300

    first commit

---
+++ atividade.ts
@@ -0,0 +1,11 @@
+function multiplicaDoisNumeros(numero1: number, numero2: number) {
+  return numero1 * numero2;
+}
+
+console.log(multiplicaDoisNumeros(2, 5));
+
+function dizOla(nome: string) {
+  return `Olá ${nome}`;
+}
+
+console.log(dizOla("Shiro"));
